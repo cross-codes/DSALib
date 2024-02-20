5cd85f0690da81e159eaf9bf50bb8bbb0ae92b24
Akshaj Rao <akshajrao2004@gmail.com>
2024-02-20 21:11:56 +0530

    feat(proj): add new solutions

--- src/striver/_3_arrays/_1/_5_rotateArray.java
+++ src/striver/_3_arrays/_1/_5_rotateArray.java
@@ -1,26 +1,40 @@
 package striver._3_arrays._1;
 
 public class _5_rotateArray {
-  public static void swap(int[] arr, int idx1, int idx2) {
-    int temp = arr[idx1];
-    arr[idx1] = arr[idx2];
-    arr[idx2] = temp;
+  public static int swapStore(int x, int[] arr, int idx) {
+    int temp2 = arr[idx];
+    arr[idx] = x;
+    return temp2;
   }
 
-  // Code to move the last element to the beginning
-  public static void rotateOnePermutation(int[] arr) {
-    for (int i = arr.length - 1; i > 0; i--) swap(arr, i, i - 1);
-  }
-
-  public static void rotate(int[] nums, int k) {
-    for (int i = nums.length - k; i < nums.length; i++) rotateOnePermutation(nums);
-  }
-
-  public static void main(String[] args) {
-    int[] testArr = {3, 1, 2, 5};
-    rotateOnePermutation(testArr);
-    for (int i = 0; i < testArr.length; i++) {
-      System.out.println(testArr[i]);
+  public static void rotateOnce(int[] arr, int length) {
+    int temp = arr[length - 1];
+    int idxTemp = arr[0];
+    for (int i = 0; i < length - 1; i++) {
+      idxTemp = swapStore(idxTemp, arr, i + 1);
     }
+    arr[0] = temp;
+  }
+
+  public static void rotateBad(int[] arr, int k) {
+    int length = arr.length;
+    for (int i = 0; i < k; i++) rotateOnce(arr, length);
+  }
+
+  public static void reverse(int[] arr, int idxStart, int idxEnd) {
+    for (; idxStart < idxEnd; idxStart++, idxEnd--) {
+      int temp = arr[idxStart];
+      arr[idxStart] = arr[idxEnd];
+      arr[idxEnd] = temp;
+    }
+  }
+
+  public static void rotate(int[] arr, int k) {
+    int length = arr.length;
+    k = k % length;
+    if (k < 0) k += length;
+    reverse(arr, 0, length - k - 1);
+    reverse(arr, length - k, length - 1);
+    reverse(arr, 0, length - 1);
   }
 }
