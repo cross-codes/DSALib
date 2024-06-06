bc7fa11822afc366f099cc7cd4259433e8cf352a
Akshaj Rao <akshajrao2004@gmail.com>
2024-06-06 13:31:27 +0530

    New solutions

--- src/striver/_4_binarySearch/_2/_1_squareRoot.java
+++ src/striver/_4_binarySearch/_2/_1_squareRoot.java
@@ -2,6 +2,15 @@ package striver._4_binarySearch._2;
 
 public class _1_squareRoot {
   public static long floorSqrt(long x) {
-    long[] possible = new long[x / 2];
+    if (x == 1) return 1;
+    long L = 0, R = x - 1, m;
+    while (L != R) {
+      if ((L + R) % 2 != 0) m = (L + R) / 2 + 1;
+      else m = (L + R) / 2;
+
+      if (m * m > x) R = m - 1;
+      else L = m;
+    }
+    return L;
   }
 }
---
+++ src/striver/_4_binarySearch/_2/_4_mBouquets.java
@@ -0,0 +1,5 @@
+package striver._4_binarySearch._2;
+
+public class _4_mBouquets {
+  public static int minDays(int[] bloomDay, int m, int k) {}
+}
